027219fc2a5535a19e8a4df489b2030ea7f20a1d
tylernguyen <tnguyen28@luc.edu>
2019-02-20 21:30:51 -0600

    populating use test class

--- src/view/MainClient.java
+++ src/view/MainClient.java
@@ -2,6 +2,8 @@ package view;
 
 public class MainClient {
     public static void main(String[] args) throws Exception{
-
+        FacilityTestClient ftc = new FacilityTestClient();
+        MaintenanceTestClient mtc = new MaintenanceTestClient();
+        FacilityUseTestClient utc = new FacilityUseTestClient();
     }
 }
